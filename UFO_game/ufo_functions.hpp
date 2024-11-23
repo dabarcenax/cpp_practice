d68f1e51f1ad9b80a55ff06d11c0933ed09087b4
dabarcenax <dabarcenax@gmail.com>
2024-11-24 00:07:28 +0100

    Add UFO game to C++ practice repository

---
+++ UFO_game/ufo_functions.hpp
@@ -0,0 +1,8 @@
+#include <vector>
+#include <string>
+
+// Declared functions
+void display_misses(int misses);
+void greet();
+void end_game(std::string codeword, std::string answer);
+void display_status(std::vector<char> incorrect, std::string answer);
